12a8b322be209161b6aecdc5840bcadb813598cf
Nandhubala@15 <nandhuvb1997@gmail.com>
2022-12-05 13:53:12 +0530

    Added extra code

---
+++ src/org/github/ExrtraCode.java
@@ -0,0 +1,15 @@
+package org.github;
+
+public class ExrtraCode {
+	
+	private void Hello() {
+  System.out.println("Hello World");
+	}
+	
+	public static void main(String[] args) {
+		ExrtraCode c = new ExrtraCode();
+				c.Hello();
+		
+	}
+
+}
